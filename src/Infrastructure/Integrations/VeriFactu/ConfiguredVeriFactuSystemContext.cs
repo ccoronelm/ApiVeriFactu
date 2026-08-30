0932849b41dc8efa9ab637507991e76956505a28
ccoronelm <102467739+ccoronelm@users.noreply.github.com>
2026-08-30 16:16:30 +0200

    Implementar contexto del sistema configurado

---
+++ src/Infrastructure/Integrations/VeriFactu/ConfiguredVeriFactuSystemContext.cs
@@ -0,0 +1,21 @@
+using gesFactu.Application.Common.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace gesFactu.Infrastructure.Integrations.VeriFactu;
+
+public sealed class ConfiguredVeriFactuSystemContext : IVeriFactuSystemContext
+{
+    private readonly VeriFactuOptions _options;
+
+    public ConfiguredVeriFactuSystemContext(IOptions<VeriFactuOptions> options)
+    {
+        _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+    }
+
+    public string ProducerName => _options.SistemaInformatico.NombreRazon;
+    public string ProducerNif => _options.SistemaInformatico.Nif;
+    public string SystemName => _options.SistemaInformatico.NombreSistemaInformatico;
+    public string SystemId => _options.SistemaInformatico.IdSistemaInformatico;
+    public string Version => _options.SistemaInformatico.Version;
+    public string InstallationNumber => _options.SistemaInformatico.NumeroInstalacion;
+}
